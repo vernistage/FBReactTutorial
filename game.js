0b1081c6d97c67564bb1e566faf95ac4f69dadd8
vernistage <sdickers10@gmail.com>
2016-12-08 21:07:23 -0600

    refactor square to be stateless component

--- game.js
+++ game.js
@@ -1,17 +1,9 @@
-class Square extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      value: null,
-    };
-  }
-  render() {
+function Square(props) {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
+      <button className="square" onClick={() => props.onClick()}>
+        {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
