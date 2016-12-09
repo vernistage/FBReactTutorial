0a2ec67bd78023fac1bc127e523f3802a02ffa62
vernistage <sdickers10@gmail.com>
2016-12-09 08:17:07 -0600

    debug to show next user

--- game.js
+++ game.js
@@ -10,27 +10,23 @@ class Board extends React.Component {
   constructor() {
     super();
     this.state = {
-      xIsNext: true
-    }
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    };
   }
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
+      xIsNext: !this.state.xIsNext,
     });
-  }
-  constructor() {
-    super();
-    this.state = {
-      squares: Array(9).fill(null),
-    };
   }
   renderSquare(i) {
     return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
   }
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player:' + (this.state.xIsNext ? 'X' : 'O');
     return (
       <div>
         <div className="status">{status}</div>
